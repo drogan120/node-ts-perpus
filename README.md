a1dd2812d2329a92f23b89b5a3a45c1926263f90
drogan120 <alimahmudin.hp@gmail.com>
2020-11-10 21:20:52 +0700

    fixing public dir and adding readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Node js Perpus
+
+### Installation
+
+<ol>
+    <li>clone this repo <code>git clone https://github.com/drogan120/{repo_name}</code></li>
+    <li>change directory with <code>cd directory_name</code></li>
+    <li>run <code>pnpm i</code>(recomemded) or <code>yarn</code> or <code>npm install</code></li>
+    <li>run <code>pnpm start</code>(recomended) or <code>yarn start</code> or <code>npm run start</code> to run the app</li>
+</ol>
+
+#### This app was build with
+<p>
+<ol>
+    <li>Node js with Typescript</li>
+    <li>Sequelize</li>
+    <li>Tailwindcss</li>
+<ol>
+</p>
--- src/app.ts
+++ src/app.ts
@@ -4,34 +4,40 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotEnv from 'dotenv'
 import path from 'path'
+
 // Routes
 import { RouteBook } from './routes'
 
 
 class App {
 
-    public app;
+    public app: Application;
 
     constructor() {
         this.app = express()
+        this.views()
         this.routes()
         this.plugins()
         dotEnv.config()
     }
 
+    protected views() {
+        this.app.use(express.static(path.join(__dirname, 'public')))
+        this.app.set('views', path.join(__dirname, 'views'))
+        this.app.set('view engine', 'ejs')
+    }
     protected plugins() {
 
         this.app.use(morgan("dev"))
         this.app.use(bodyParser.json())
         this.app.use(cors)
-        this.app.use(express.static(path.join(__dirname, 'public')))
-        this.app.set('view engine', 'ejs')
-        this.app.set('views', path.join(__dirname, 'views'))
-
 
     }
 
     protected routes() {
+        this.app.get('/', function (req, res) {
+            res.render('books/index')
+        })
         this.app.use('/books', RouteBook)
     }
 
