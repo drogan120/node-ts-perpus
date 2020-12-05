e31454189bec5bc9d7ec3fcc09ed15454b8b9f69
drogan120 <alimahmudin.hp@gmail.com>
2020-12-05 22:55:37 +0700

    adding detail page and change home page

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
 
+const { Op } = require('sequelize');
 const db = require('../db/models');
-const { Op } = require('sequelize')
+
 class HomeController {
   index = async (req: Request, res: Response): Promise<void> => {
     res.render('home/index');
@@ -20,6 +21,22 @@ class HomeController {
       res.render('home/index', { data });
     } catch (e) {
       res.send(e);
+    }
+  }
+
+  bookInfo = async (req:Request, res:Response):Promise<void> => {
+    try {
+      const { isbn } = req.params;
+      const book = await db.book.findOne({
+        where: {
+          isbn: {
+            [Op.like]: `%${isbn}%`,
+          },
+        },
+      });
+      res.render('home/booksdetail', { book });
+    } catch (err) {
+      res.send(err);
     }
   }
 }
--- src/routes/HomeRoutes.ts
+++ src/routes/HomeRoutes.ts
@@ -7,6 +7,7 @@ class HomeRoutes extends BaseRoutes {
   public routes() {
     this.router.get('/', Home.index);
     this.router.post('/', Home.search);
+    this.router.get('/books/:isbn', Home.bookInfo);
   }
 }
 export default new HomeRoutes().router;
